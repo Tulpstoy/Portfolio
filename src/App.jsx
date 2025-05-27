6e237867916d1f9b9c2164029d600332c6a56cbb
Tulpstoy <micah.bron@icloud.com>
2025-05-27 14:33:19 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./global/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
-import Project1Page from "./assets/Project1/Project1Page";
+import Project1Page from "./assets/project1/Project1Page";
 import Project2Page from "./assets/project2/Project2Page";
 import Project3Page from "./assets/project3/Project3Page";
 import Project4Page from "./assets/project4/Project4Page";
